7db7e29b3626582390e4aa2dee71521270523755
Valery Zazulin <zazulinvalerii@gmail.com>
2024-03-14 19:02:48 +0100

    New Commit

--- SparrowCodeSwiftUIBootcamp_Task6/MovingSquares.swift
+++ SparrowCodeSwiftUIBootcamp_Task6/MovingSquares.swift
@@ -9,57 +9,50 @@ import SwiftUI
 
 struct MovingSquares: View {
     
-    let quantity: Int = 9
+    let quantity: CGFloat = 8
     let spacing: CGFloat = 10
-    @State var isDiagonalMode: Bool = false
+    @State var isDiagonal: Bool = false
     
     var body: some View {
         GeometryReader { geo in
-            let squareHeight = (geo.size.width - (spacing * CGFloat(quantity) - 1)) / CGFloat(quantity)
-            let bigSquareWidth = geo.size.height / CGFloat(quantity)
+            let squareHeight = (geo.size.width - (spacing * (quantity - 1))) / quantity
+            let bigSquareWidthAndHeight = geo.size.height / quantity
+            let newSpacing = (geo.size.width - bigSquareWidthAndHeight * quantity) / (quantity - 1)
+            let isEven = Int(quantity) % 2 == 0
             
-            HStack(spacing: isDiagonalMode ? spacing : ((geo.size.width - bigSquareWidth * CGFloat(quantity)) / CGFloat(quantity - 1))) {
-                ForEach(0..<quantity) { index in
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(Color.blue)
-                        .frame(
-                            width: isDiagonalMode ? squareHeight : geo.size.height / CGFloat(quantity),
-                            height: isDiagonalMode ? squareHeight : geo.size.height / CGFloat(quantity)
-                        )
-                        .offset(
-                            y: isDiagonalMode ? 0 :
-                                (
-                                    quantity % 2 == 0 ?
-                                    -bigSquareWidth / 2 + bigSquareWidth * -findOffsetMultiplier(quantity: quantity, index: index) :
-                                        bigSquareWidth * -findOffsetMultiplier(quantity: quantity, index: index)
-                                )
-                    )
-                        .onTapGesture {
-                            withAnimation {
-                                isDiagonalMode.toggle()
+                HStack(spacing: isDiagonal ? spacing : newSpacing) {
+                    ForEach(0..<Int(quantity), id: \.self) { index in
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(Color.blue)
+                            .frame(
+                                width: isDiagonal ? squareHeight : bigSquareWidthAndHeight,
+                                height: isDiagonal ? squareHeight : bigSquareWidthAndHeight
+                            )
+                            .offset(y: isDiagonal ? 0 :
+                                    findOffset(quantity: quantity,
+                                               index: index,
+                                               isEven: isEven,
+                                               bigSquareWidthAndHeight: bigSquareWidthAndHeight)
+                            )
+                            .onTapGesture {
+                                withAnimation {
+                                    isDiagonal.toggle()
+                                }
                             }
-                        }
+                    }
                 }
-                
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
-        
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .padding()
-        
-        
     }
     
-    func findOffsetMultiplier(quantity: Int, index: Int) -> CGFloat {
-        let middleIndex = (quantity + 1) / 2
-        
+    func findOffset(quantity: CGFloat, index: Int, isEven: Bool, bigSquareWidthAndHeight: CGFloat) -> CGFloat {
+        let middleIndex = (Int(quantity) + 1) / 2
         let distance = index - middleIndex + 1
         
-        if quantity % 2 == 0 {
-            return CGFloat(-middleIndex + index)
+        if isEven {
+            return -bigSquareWidthAndHeight / 2 + bigSquareWidthAndHeight * -CGFloat(-middleIndex + index)
         } else {
-            return CGFloat(distance)
+            return bigSquareWidthAndHeight * -CGFloat(distance)
         }
     }
 }
